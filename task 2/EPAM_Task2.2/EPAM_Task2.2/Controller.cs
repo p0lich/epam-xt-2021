b3ef26826878a946cc720fade8b003325ed5cb4d
Aleksandr <a.polikarpov2014@gmail.com>
2021-03-18 21:50:29 +0400

    Task 2.2

---
+++ task 2/EPAM_Task2.2/EPAM_Task2.2/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPAM_Task2._2
+{
+    public class Controller
+    {
+        public event EventHandler pressLeftKey;
+        public event EventHandler pressUpKey;
+        public event EventHandler pressRightKey;
+        public event EventHandler pressDownKey;
+
+        public void StartListen()
+        {
+            while(true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if(key.Key.Equals(ConsoleKey.LeftArrow))
+                {
+                    pressLeftKey?.Invoke(this, new EventArgs());
+                }
+
+                else if (key.Key.Equals(ConsoleKey.UpArrow))
+                {
+                    pressUpKey?.Invoke(this, new EventArgs());
+                }
+
+                else if (key.Key.Equals(ConsoleKey.RightArrow))
+                {
+                    pressRightKey?.Invoke(this, new EventArgs());
+                }
+
+                else if (key.Key.Equals(ConsoleKey.DownArrow))
+                {
+                    pressDownKey?.Invoke(this, new EventArgs());
+                }
+
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
