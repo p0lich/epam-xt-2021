4d5c6e6a549541b17ff223460a4aa6eac80892c0
DESKTOP-3D0F6AC\Alex <a.polikarpov2014@gmail.com>
2021-03-26 18:33:52 +0400

    Task 3.2(uncompleted)

---
+++ task 3/EPAM_Task3.2/EPAM_Task3.2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExtendedCollections;
+
+namespace EPAM_Task3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray<int> mass = new DynamicArray<int>(new int[] { 1, 2, 3, 4 });
+            ShowInfo(mass);
+
+            mass.Add(10);
+            ShowInfo(mass);
+
+            mass.AddRange(new int[] { 11, 12, 13, 14 });
+            ShowInfo(mass);
+
+            mass.Remove(2);
+            ShowInfo(mass);
+
+            mass.Insert(8, -1);
+            ShowInfo(mass);
+
+            mass.Insert(0, -2);
+            ShowInfo(mass);
+
+            //List<int> lst = new List<int>();
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    lst.Add(i);
+            //}
+
+            //lst.Insert(4, 9);
+
+            //for (int i = 0; i < lst.Count; i++)
+            //{
+            //    Console.WriteLine(lst[i]);
+            //}
+        }
+
+        public static void ShowInfo<T>(DynamicArray<T> mass)
+        {
+            StringBuilder str = new StringBuilder();
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                str.Append(mass[i] + " ");
+            }
+
+            Console.WriteLine("Data: {0}\nLength: {1}\nCapacity: {2}\n",
+                str.ToString(),
+                mass.Length,
+                mass.Capacity);
+        }
+    }
+}
---
+++ task 3/EPAM_Task3.2/ExtendedCollections/DynamicArray.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExtendedCollections
+{
+    public class DynamicArray<T> : IEnumerable<T>, IEnumerable
+    {
+        private T[] _data;
+
+        public int Length { get; private set; }
+
+        public int Capacity
+        {
+            get
+            {
+                return _data.Length;
+            }
+        }
+
+        public DynamicArray()
+        {
+            _data = new T[8];
+            Length = 0;
+        }
+
+        public DynamicArray(int length)
+        {
+            _data = new T[length];
+            Length = 0;
+        }
+
+        public DynamicArray(IEnumerable<T> collection)
+        {
+            RewriteData(ref _data, out int length, collection);
+            Length = length;
+        }
+
+        public T this[int index]
+        {
+            // TODO: ArgumentOutOfRangeException
+            get
+            {
+                if (index >= 0)
+                {
+                    return _data[index];
+                }
+
+                return _data[Length + index];
+            }
+        }
+
+        public void Add(T value)
+        {
+            if (Length == Capacity)
+            {
+                IncreaseCapacity();
+            }
+
+            _data[Length] = value;
+            Length++;
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            int newLength = GetCollectionLength(collection) + Length;
+
+            if (newLength > Capacity)
+            {
+                IncreaseCapacity(newLength);
+            }
+
+            foreach (var item in collection)
+            {
+                _data[Length] = item;
+                Length++;
+            }
+        }
+
+        public bool Remove(int index)
+        {
+            if (index < Length)
+            {
+                ArrayOffset(index, false);
+
+                _data[Length - 1] = default;
+                Length--;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Insert(int index, T value)
+        {
+            // TODO: ArgumentOutOfRangeException
+
+            if (index <= Length)
+            {
+                if (Length == Capacity)
+                {
+                    IncreaseCapacity();
+                }
+
+                Length++;
+                ArrayOffset(index, true);
+                _data[index] = value;
+                
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private static int GetCollectionLength(IEnumerable<T> collection)
+        {
+            int length = 0;
+
+            foreach (var item in collection)
+            {
+                length++;
+            }
+
+            return length;
+        }
+
+        private void IncreaseCapacity(int collectionSize)
+        {
+            //while (Capacity < collectionSize)
+            //{
+            //    IncreaseCapacity();
+            //}
+
+            T[] tempData = _data;
+            _data = new T[collectionSize];
+
+            for (int i = 0; i < tempData.Length; i++)
+            {
+                _data[i] = tempData[i];
+            }
+        }
+
+        private void IncreaseCapacity()
+        {
+            T[] tempData = _data;
+            _data = new T[Capacity * 2];
+
+            for (int i = 0; i < tempData.Length; i++)
+            {
+                _data[i] = tempData[i];
+            }
+        }
+
+        private static void RewriteData(ref T[] data, out int length, IEnumerable<T> collection)
+        {
+            length = 0;
+            List<T> tempData = new List<T>();
+
+            foreach (var item in collection)
+            {
+                length++;
+                tempData.Add(item);
+            }
+
+            data = tempData.ToArray();
+        }
+
+        private void ArrayOffset(int referenceIndex, bool isAdd)
+        {
+            if (isAdd)
+            {
+                for (int i = Length - 2; i >= referenceIndex; i--)
+                {
+                    _data[i + 1] = _data[i];
+                }
+            }
+            
+            else
+            {
+                for (int i = referenceIndex; i < Length - 1; i++)
+                {
+                    _data[i] = _data[i + 1];
+                }
+            }
+        }
+
+        // TODO
+        #region INTERFACE_IMPLEMENTATION
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
