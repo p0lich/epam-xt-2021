10f82bbb57ab657b73f56e3fdb5f019a33ff4ca8
Aleksandr <a.polikarpov2014@gmail.com>
2021-04-13 13:06:36 +0400

    Task 3.3.3 test changes

--- task 3/EPAM_Task3.3.3/EPAM_Task3.3.3/Pizzeria.cs
+++ task 3/EPAM_Task3.3.3/EPAM_Task3.3.3/Pizzeria.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EPAM_Task3._3._3
@@ -14,25 +15,49 @@ namespace EPAM_Task3._3._3
         public List<Order> ProcessingOrders { get; private set; }
         public List<Order> ReadyToTakeOrders { get; private set; }
 
-        public Pizzeria(string name, List<Pizza> menu)
+        public int WorkingTime { get; }
+
+        public Pizzeria(string name, List<Pizza> menu, int workingTime)
         {
             Name = name;
             Menu = menu;
             LastOrder = 0;
             ProcessingOrders = new List<Order>();
             ReadyToTakeOrders = new List<Order>();
+            WorkingTime = workingTime;
+        }
+
+        // Метод запускает отдельный поток под работу пиццерии.
+        // Заказы будут выполнятся до тех пор, пока не закончится время
+        public void StartWorking()
+        {
+            Task.Factory.StartNew(() => WorkingProcess(ReadyOrderNotify));
         }
 
         // 0 - all ok
         // -1 - order is empty
         // -2 - not enough funds
+        // -3 - working time is over
+        // -4 - not enought time for cooking
+
         public int MakeOrder(Client client, ref int clientBalance)
         {
+            if (WorkingTime == 0)
+            {
+                return -3;
+            }
+
             List<Pizza> pizzasForOrder = ChoosePizzas();
+            int preparingTime = GetPreparingTime(pizzasForOrder);
 
             if (pizzasForOrder.Count == 0)
             {
                 return -1;
+            }
+
+            if (preparingTime > WorkingTime)
+            {
+                return -4;
             }
 
             int totalPrice = GetPrice(pizzasForOrder);
@@ -54,6 +79,7 @@ namespace EPAM_Task3._3._3
             return 0;
         }
 
+        // Старая версия. Заказы обрабатываются по одному в порядке поступления
         public void WorkingProcess(int timeSpend)
         {
             if (ProcessingOrders.Count > 0)
@@ -74,6 +100,67 @@ namespace EPAM_Task3._3._3
                     WorkingProcess(leftTime);
                 }
             }
+
+            //Thread.Sleep(5000);
+            //func("vasya", new List<Pizza>());
+        }
+
+        // Новая версия. Работает одновременно со всеми заказами,
+        // которые могут поступать в разное время
+        private void WorkingProcess(Action<int> action)
+        {
+            Console.WriteLine("Pizzeria \"" + Name + "\" has opened");
+            int leftTime = WorkingTime;
+
+            while (leftTime > 0)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                leftTime--;
+
+                if (ProcessingOrders.Count <= 0)
+                {
+                    continue;
+                }
+
+                ProcessingOrders.ForEach(v => v.PreparingTime--);
+
+                if (ProcessingOrders.All(v => v.PreparingTime > 0))
+                {
+                    continue;
+                }
+
+                var readyOrders = ProcessingOrders.Where(v => v.PreparingTime == 0);
+
+                foreach (var item in readyOrders.ToList())
+                {
+                    action?.Invoke(item.OrderNumber);
+
+                    ReadyToTakeOrders.Add(item);
+                    ProcessingOrders.Remove(item);
+                }
+            }
+
+            Console.WriteLine("Pizzeria \"" + Name + "\" has closed");
+            ProcessingOrders.Clear();
+
+            if (ReadyToTakeOrders.Count > 0)
+            {
+                Console.WriteLine("Orders: ");
+
+                foreach (var item in ReadyToTakeOrders)
+                {
+                    Console.Write(item.OrderNumber + " ");
+                }
+
+                Console.WriteLine("\nHave not been taken and will be discarded");
+
+                ReadyToTakeOrders.Clear();
+            }
+        }
+
+        private static void ReadyOrderNotify(int orderNumber)
+        {
+            Console.WriteLine("Order number " + orderNumber + " is ready");
         }
 
         public bool GiveAway(Client client)
@@ -164,6 +251,18 @@ namespace EPAM_Task3._3._3
             }
 
             Console.WriteLine((Menu.Count + 1).ToString() + " - Exit");
+        }
+
+        private static int GetPreparingTime(List<Pizza> pizzas)
+        {
+            int totalTime = 0;
+
+            for (int i = 0; i < pizzas.Count; i++)
+            {
+                totalTime += pizzas[i].CookingTime;
+            }
+
+            return totalTime;
         }
     }
 }
